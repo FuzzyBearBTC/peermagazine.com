eb7b2cdc5781e601a0f39d8c85fb186c4d1c6c6c
hoschiCZ <ondrej.sojka@gmail.com>
2014-10-30 20:36:10 +0100

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,6 @@
   <meta charset="utf-8">
   <style>
     articleheader {font-size: big; color: blue}
-    body {margin: 50px; font-family: sans-serif;}
+    body {margin: 50px; font-family: sans-serif; border: 5px solid gray}
   </style>
 </head>
