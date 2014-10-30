6b02e4d016abc11cdf60eb5a419f996f785c07ad
hoschiCZ <ondrej.sojka@gmail.com>
2014-10-30 20:08:31 +0100

    added echoing of last 5 articles

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 include("header.php");
 ?>
-<articleheader>Peermagazine project launched!</articleheader><br>
-<text>We are so happy!</text>
+<?php
+$lastarticlefile=fopen("lastarticle.dat", "r"); //here is stored info about last article - that number
+$lastarticlenumber = fread($lastarticlefile, filesize($lastarticlefile));
+fclose($lastarticlefile);
+$thisarticlefile = fopen("articles/".$lastarticlenumber,"r");
+echo(fread($thisarticlefile,filesize($thisarticlefile))); //now, last article is echoed. printing last 5 articles (only previews, and these previews include anchor to full article by fullarticle.php)
+fclose($thisarticlefile);
+$echoedarticles = 1;
+while($echoedarticles < 6){
+  if($lastarticlenumber > -1){
+    $lastarticlenumber = $lastarticlenumber-1;
+    $thisarticlefile = fopen("articles/".$lastarticlenumber,"r");
+    echo(fread($thisarticlefile,filesize($thisarticlefile))); //now, last article is echoed. printing last 5 articles (only previews, and these previews include anchor to full article by fullarticle.php)
+    fclose($thisarticlefile);
+  }
+  $echoedarticles = $echoedarticles + 1;
+}
+//now, last 5 articles (previews) are echoed.
+?>
 </body>
 </html>
