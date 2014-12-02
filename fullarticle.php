a71842992eaab63316ca3317ca9e6ec0d014bb17
hoschiCZ <ondrej.sojka@gmail.com>
2014-12-02 16:12:15 +0100

    bugfixes

--- fullarticle.php
+++ fullarticle.php
@@ -1,11 +1,11 @@
 <?php
 require("header.php"); //require is include which gives an error if file can't be found
-$articleid = $_POST['id'];
-if(empty($_POST['id'])){
-  echo('You didn\'t specify id of article you wish to view.')
+$articleid = $_GET['id'];
+if(!is_numeric(intval($articleid))){
+  echo('You didn\'t specify id of article you wish to view.');
 }
 $articlefile=fopen("fullarticle/".$articleid, "r");
-echo(fread($articlefile,filesize($articlefile))); //content of file echoed
+echo(fread($articlefile,filesize('fullarticle/'.$articleid))); //content of file echoed
 ?>
 
 </body>
